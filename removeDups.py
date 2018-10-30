3c46bd7b20073e2f0c7ca6810cb5565de9a2b50e
Nicholas West <nwest13@vols.utk.edu>
2018-10-30 13:07:31 -0400

    More cleanup

--- removeDups.py
+++ removeDups.py
@@ -1,14 +1,12 @@
 import json
 import socket
 import sys
-import pymongo
-from pymongo import MongoClient
 
-def usage():
+from pymongo import MongoClient
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: %s [MongoDB collection]" % sys.argv [0], file = sys.stderr)
+        print("Usage: %s [MongoDB collection]" % sys.argv[0], file = sys.stderr)
         exit(-1)
 
     conn = MongoClient("da1.eecs.utk.edu" if socket.gethostname() == "75f7e392a7ec" else "localhost")
