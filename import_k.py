43e33b8ef0b73021e5c54d72479e073e3886cecf
Nicholas West <nwest13@vols.utk.edu>
2018-10-31 08:59:08 -0400

    ensure_index is deprecated

--- import_k.py
+++ import_k.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     coll = conn['twitter'][output_coll]
 
     # Make tweets indexable by id and text fields
-    coll.ensure_index([('id', pymongo.ASCENDING)], name = 'id_index')
-    coll.ensure_index([('text', pymongo.TEXT)], name = 'search_index', default_language = 'english')
+    coll.create_index([('id', pymongo.ASCENDING)], name = 'id_index')
+    coll.create_index([('text', pymongo.TEXT)], name = 'search_index', default_language = 'english')
 
     for filename in input_files:
         map_f    = lambda ln: json.loads(ln.strip()) if ln.strip() else {}
