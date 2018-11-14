a1858bef30ddebc8c763cdc071c74f29ccd21d74
Nicholas West <nwest13@vols.utk.edu>
2018-11-14 09:59:45 -0500

    Zipped retrieval and look up processes

---
+++ import_labelled.py
@@ -0,0 +1,159 @@
+import csv
+import copy
+import datetime
+import contextlib
+import sys
+import itertools
+import pickle
+from email.utils import format_datetime
+
+import nltk
+import pymongo
+from fuzzywuzzy import process
+import tweepy
+
+TWITTER_AUTH = tweepy.OAuthHandler(
+    "ZFVyefAyg58PTdG7m8Mpe7cze",
+    "KyWRZ9QkiC2MiscQ7aGpl5K2lbcR3pHYFTs7SCVIyxMlVfGjw0"
+)
+TWITTER_AUTH.set_access_token(
+    "1041697847538638848-8J81uZBO1tMPvGHYXeVSngKuUz7Cyh",
+    "jGNOVDxllHhO57EaN2FVejiR7crpENStbZ7bHqwv2tYDU"
+)
+
+def extended_to_compat(status, status_permalink = None):
+    r = copy.deepcopy(status)
+
+    full_text = r["full_text"]
+    entities = r["entities"]
+
+    r["extended_tweet"] = {
+        "full_text": r["full_text"],
+        "display_text_range": r["display_text_range"],
+        "entities": r["entities"]
+    }
+
+    del r["full_text"]
+    del r["display_text_range"]
+
+    if "extended_entities" in r:
+        r["extended_tweet"]["extended_entities"] = r["extended_entities"]
+        del r["extended_entities"]
+
+    if len(full_text) > 140:
+        r["truncated"] = True
+
+        if status_permalink is None:
+            long_url = "https://twitter.com/tweet/web/status/" + r["id_str"]
+
+            # Use TinyURL to shorten link to tweet
+            with urlopen('http://tinyurl.com/api-create.php?' + urlencode({'url': long_url})) as response:
+                short_url = response.read().decode()
+
+            status_permalink = {
+                "url": short_url,
+                "expanded_url": long_url,
+                "display_url": "twitter.com/tweet/web/status/\u2026",
+                "indices": [140 - len(short_url), 140]
+            }
+        else:
+            short_url = status_permalink["url"]
+            status_permalink["indices"] = [140 - len(short_url), 140]
+
+        r["text"] = full_text[:(138 - len(short_url))] + "\u2026 " + short_url
+
+        r["entities"] = {
+            "hashtags": [],
+            "symbols": [],
+            "user_mentions": [],
+            "urls": [status_permalink]
+        }
+
+        for k in r["entities"].keys():
+            for v in entities[k]:
+                if v["indices"][1] <= 138 - len(short_url):
+                    r["entities"][k].append(v)
+
+    else:
+        r["text"] = full_text
+        r["entities"] = {k: entities[k] for k in ("hashtags", "symbols", "user_mentions", "urls")}
+
+    if "quoted_status" in r:
+        if "quoted_status_permalink" in r:
+            quoted_status_permalink = r["quoted_status_permalink"]
+            del r["quoted_status_permalink"]
+        else:
+            quoted_status_permalink = None
+
+        r["quoted_status"] = extended_to_compat(r["quoted_status"], quoted_status_permalink)
+
+    return r
+
+if __name__ == "__main__":
+    csvin = csv.DictReader(sys.stdin, dialect = csv.unix_dialect)
+
+    tweets = list(csvin)
+
+    csvout = csv.DictReader(sys.stdout, fieldnames = csvin.fieldnames, dialect = csv.unix_dialect)
+
+    with contextlib.closing(pymongo.MongoClient()) as conn:
+        coll = conn['twitter']['rawCIrma']
+        api = tweepy.API(TWITTER_AUTH, parser = tweepy.parsers.JSONParser())
+
+        def relookup(tweet):
+            query = {"$text": {"$search": " ".join(nltk.tokenize.wordpunct_tokenize(tweet["Tweet"].strip().lower()))}}
+            match_map = {r["text"]: r["_id"] for r in coll.find(query, projection = ["text"])}
+
+            match, dist = process.extractOne(tweet["Tweet"], list(match_map.keys()))
+
+            if dist < 80:
+                print("Failed to find good match for tweet " + repr(tweet), file = sys.stderr)
+                return None
+
+            r = coll.find_one({"_id": match_map[match]})
+
+            try:
+                new_tweet = api.get_status(
+                    r["id"],
+                    tweet_mode = "extended",
+                    include_entities = True,
+                    monitor_rate_limit = True,
+                    wait_on_rate_limit = True
+                )
+
+                timestamp = format_datetime(datetime.datetime.utcnow().replace(tzinfo = datetime.timezone.utc))
+
+            except KeyError:
+                print("Failed to get tweet ID for " + repr(r), file = sys.stderr)
+                return None
+
+            except tweepy.Error:
+                print("Error with Twitter API (perhaps tweet is deleted?)", file = sys.stderr)
+                return None
+
+            new_tweet = extended_to_compat(new_tweet)
+            new_tweet["retrieved_at"] = timestamp
+            new_tweet["code"] = tweet["Manual Coding"]
+
+            print("'%s' -> '%s' (%d%% match)" % (tweet["Tweet"], new_tweet["text"], dist), file = sys.stderr)
+
+            return new_tweet
+
+        tweets = list(zip(tweets, map(relookup, tweets)))
+
+        successes = [new_tweet for tweet, new_tweet in tweets if new_tweet is not None]
+        failures = [tweet for tweet, new_tweet in tweets if new_tweet is None]
+
+        coll_labeled = conn['twitter']['labeledAIrma']
+
+        coll_labeled.create_index([('id', pymongo.HASHED)], name = 'id_index')
+        coll_labeled.create_index([('id', pymongo.ASCENDING)], name = 'id_ordered_index')
+        coll_labeled.create_index([('text', pymongo.TEXT)], name = 'search_index', default_language = 'english')
+
+        coll_labeled.insert_many(successes)
+
+    print("The following tweets could not be added:", file = sys.stderr)
+
+    csvout.writeheader()
+    for f in failures:
+        csvout.writerow(f)
