6ec2b6cb9cce67ab10319e30f44e5cd166c0c956
Nicholas West <nwest13@vols.utk.edu>
2019-03-21 11:06:49 -0400

    Removed SIFT and SURF from images

--- images/main.py
+++ images/main.py
@@ -12,7 +12,29 @@ import cv2
 import tweepy
 
 from common import *
-from points import get_orb, get_sift, get_surf
+
+def keypoint2dict(kp):
+    return {
+        "x": kp.pt[0],
+        "y": kp.pt[1],
+        "size": kp.size,
+        "angle": kp.angle,
+        "response": kp.response,
+        "octave": kp.octave,
+        "class_id": kp.class_id
+    }
+
+# Will probably be useful later
+def dict2keypoint(d):
+    return cv2.KeyPoint(
+        d["x"],
+        d["y"],
+        d["size"],
+        d["angle"],
+        d["response"],
+        d["octave"],
+        d["class_id"]
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -26,6 +48,7 @@ if __name__ == "__main__":
         coll_to = exitstack.enter_context(opencoll(db, sys.argv[2]))
 
         colldir = posixpath.join("/", "home", "nwest13", "Media", sys.argv[2][(sys.argv[2].find("_") + 1):])
+        orb = cv2.ORB_create()
 
         try:
             sftp.mkdir(colldir)
@@ -58,7 +81,8 @@ if __name__ == "__main__":
                         with urlopen(url) as response:
                             filedata = response.read()
                         break
-                    except HTTPError:
+                    except HTTPError as e:
+                        print("Error downloading %s: %r" % (url, e))
                         time.sleep(5)
 
                 filename = posixpath.join(colldir, url[(url.rfind("/") + 1):])
@@ -68,21 +92,30 @@ if __name__ == "__main__":
 
                 print("Downloaded %s from tweet %d to %s" % (url, r0["id"], filename))
 
-                with tempfile.NamedTemporaryFile() as fd:
-                    fd.write(filedata)
-                    mat = cv2.imread(fd.name, cv2.IMREAD_GRAYSCALE)
-
                 sha256sum = hashlib.sha256()
                 sha256sum.update(filedata)
 
-                medialist.append({
+                r = {
                     "remote_url": url,
                     "local_url": filename,
                     "sha256sum": sha256sum.hexdigest(),
-                    "orb": get_orb(mat) if mat is not None else None,
-                    "sift": get_sift(mat) if mat is not None else None,
-                    "surf": get_surf(mat) if mat is not None else None
-                })
+                    "keypoints": None,
+                    "descriptors": None
+                }
+
+                with tempfile.NamedTemporaryFile(delete = False) as fd:
+                    tempname = fd.name
+                    fd.write(filedata)
+
+                img = cv2.imread(tempname, cv2.IMREAD_GRAYSCALE)
+                if img is not None:
+                    kp, des = orb.detectAndCompute(img, None)
+
+                    r["keypoints"] = list(map(keypoint2dict, kp))
+                    r["descriptors"] = des.tolist()
+
+                os.remove(temp_filename)
+                medialist.append(r)
 
             coll_to.insert_one({
                 "id": r0["id"],
